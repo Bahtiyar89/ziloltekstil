d831dfb3a8c75853268123ac3fcbe6db8c6d9017
bahtiyar <bahtiyar1989@gmail.com>
2025-07-18 03:34:34 +0300

    first commit

---
+++ src/actions/index.ts
@@ -0,0 +1,24 @@
+import { redirect } from "react-router-dom"; 
+
+interface SearchActionRequest {
+  request: {
+    formData: () => Promise<FormData>;
+  };
+}
+
+ 
+
+export const searchAction = async ({ request }: SearchActionRequest) => {
+  // getting form data
+  const formData = await request.formData();
+  // converting form data to object for easy access
+  const data = Object.fromEntries(formData);
+
+  return redirect(`/search?query=${data?.searchInput || ""}`);
+};
+
+
+export const checkoutAction = async () => {
+   
+  return redirect('/');
+}
---
+++ src/components/ProductItem.tsx
@@ -0,0 +1,64 @@
+import { Link } from "react-router-dom";
+import { formatCategoryName } from "../utils/formatCategoryName";
+import luxury1 from '../assets/luxurycategory1.png'; 
+import luxury2 from '../assets/luxurycategory2.png'; 
+import luxury3 from '../assets/luxurycategory3.png'; 
+import luxury4 from '../assets/luxurycategory4.png'; 
+import luxury5 from '../assets/luxuryfashion71.png'; 
+import luxury6 from '../assets/luxuryfashion72.png';
+
+const ProductItem = ({
+  id,
+  image,
+  title,
+  category,
+  price,
+}: {
+  id: string;
+  image: string;
+  title: string;
+  category: string;
+  price: number;
+}) => {
+
+  console.log("image",image);
+  
+
+  return (
+    <div className="w-[400px] flex flex-col gap-2 justify-center max-md:w-[300px]">
+      <Link
+        to={`/product/${id}`}
+        className="w-full h-[300px] max-md:h-[200px] overflow-hidden"
+      >
+        <img src={image=="product image 1.jpg"?luxury1:image=="product image 2.jpg"? luxury2: image=="product image 3.jpg"?luxury3:image=="product image 4.jpg"?luxury4:image=="product image 5.jpg"?luxury5:luxury6} alt={title} />
+      </Link>
+      <Link
+        to={`/product/${id}`}
+        className="text-black text-center text-3xl tracking-[1.02px] max-md:text-2xl"
+      >
+        <h2>{title}</h2>
+      </Link>
+      <p className="text-secondaryBrown text-lg tracking-wide text-center max-md:text-base">
+        {formatCategoryName(category)}{" "}
+      </p>
+      <p className="text-black text-2xl text-center font-bold max-md:text-xl">
+        {price * 3} TL
+      </p>
+      <div className="w-full flex flex-col gap-1">
+        <Link
+          to={`/product/${id}`}
+          className="text-white bg-secondaryBrown text-center text-xl font-normal tracking-[0.6px] leading-[72px] w-full h-12 flex items-center justify-center max-md:text-base"
+        >
+          View product
+        </Link>
+        <Link
+          to={`/product/${id}`}
+          className="bg-white text-black text-center text-xl border border-[rgba(0, 0, 0, 0.40)] font-normal tracking-[0.6px] leading-[72px] w-full h-12 flex items-center justify-center max-md:text-base"
+        >
+          Learn more
+        </Link>
+      </div>
+    </div>
+  );
+};
+export default ProductItem;
